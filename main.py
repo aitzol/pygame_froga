15532a887b63c795ef26ce20bc430df73787662a
aitzol <anaberan@gmail.com>
2025-12-09 06:23:33 +0100

    Add collision

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+
 pygame.init()  #inicializar todos los modulos de pygame
 pygame.font.init()
 screen = pygame.display.set_mode((800, 400))
@@ -20,6 +21,7 @@ def move(rect, direction):
 
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
+
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 left_paddle = pygame.rect.Rect(0,0,PADDLE_WIDTH, PADDLE_HEIGHT)
@@ -45,12 +47,12 @@ while True:
                 move(right_directions, event.key)
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface,  (300, 50))
     
 
     l = pygame.draw.line(screen, (255, 0, 0), (50, 50), (150, 150), 3)
     r = pygame.draw.rect(screen, (0, 255, 255), (pos_x_rect, pos_y_rect, 100, 100))
-    c = pygame.draw.circle(screen, (255, 255, 255), (pos_x_circle, pos_y_circle), 10, 10)
+    
+    c = pygame.draw.circle(screen, (255, 255, 255), (pos_x_circle, pos_y_circle), 10, 10)                     
     e = pygame.draw.ellipse(screen, (0, 255, 0), (75, 225, 150, 100))
     
     if pos_x_circle == 800:
@@ -66,8 +68,16 @@ while True:
     pos_y_circle = pos_y_circle + update_y
     pos_x_rect += update_x*1.5
     pos_y_rect += update_y*0.3
+    
     print(f'({pos_x_circle}, {pos_y_circle})')
-    pygame.draw.rect(screen, 'Black', left_paddle)
-    pygame.draw.rect(screen, 'Black', right_paddle)
+    p1=pygame.draw.rect(screen, 'Black', left_paddle)
+    p2=pygame.draw.rect(screen, 'Black', right_paddle)
+    
+
+    if c.colliderect(r):
+        text_surface = test_font.render("Colision!",  False, 'Red')
+    else:
+        text_surface = test_font.render("My game",  False, 'Green')
+    screen.blit(text_surface,  (300, 50))   
     pygame.display.update()
     clock.tick(60)
